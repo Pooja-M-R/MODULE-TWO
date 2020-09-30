4fa9a308478af414844191363d99820447400964
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-09-30 15:51:40 +0530

    Update Program-10.c

--- Program-10.c
+++ Program-10.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, small, large;
+    printf("Enter how many values you want to read : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the value of a[%d] : ",i);
+        scanf("%d", &arr[i]);
+    }
+
+    small = arr[0];
+    large = arr[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] <  )
+        {
+            small = arr[ ];
+        }
+        
+        if (arr[i] >  )
+        {
+            large = arr[ ];
+        }
+    }
+
+    printf("The largest element of the array = %d\n", large);
+    printf("The smallest element of the array = %d\n", small);
+
+    return 0;
+}
 
