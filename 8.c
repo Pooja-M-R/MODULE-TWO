45dd46e5ed4a8ca49309c968077c9fb25d24144a
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-09-30 14:56:02 +0530

    PRINTING THE FREQUENCY OF A GIVEN INTEGER IN AN ARRAY

---
+++ 8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+  int n,flag=0,i,key,a[10],c=0;
+  printf("Enter size of the array : ");
+  scanf("%d",&n);
+  printf("Enter array elements : ");
+  for(i;i<;i)
+  scanf("%d",&a[]);
+  printf("Enter an integer value : ");
+  scanf("%d",&key);
+  printf("The indexes of the array elements matching the given value are : ");
+    for(i=0;i<n;i){
+  if(key==a[ ]) {
+    flag=1;
+    c++;
+    printf("%d ",i);printf("Number of times element %d is repeated : %d\n",  , );
+  }
+    }
+    return(0);
+}
+
