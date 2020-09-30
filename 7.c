e60ef3f9a157e58133406dc06cea7560628e9cf4
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-09-30 14:53:43 +0530

    PRINTING THE ARRAY ELEMENTS IN REVERSE ORDER

---
+++ 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a[10],i,n;
+  printf("Enter size of the array : ");
+  scanf("%d",&n);
+  printf("Enter array elements : ");
+  for(i=0;i<n;i++)
+  scanf("%d",&a[]);
+  printf("Array elements in reverse order : ");
+  for(i=;i;i)
+  printf("%d ",a[i]);
+  return(0);
+}
