2c53ac173d40843bb0205a9474f367a166af80ea
Pooja-M-R <71132064+Pooja-M-R@users.noreply.github.com>
2020-09-30 15:03:18 +0530

    Transpose of a matrix

---
+++ 9.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a[5][5],i,j,m,n;
+    printf("Enter the order of matrix : ");
+    scanf("%d%d",&m,&n);
+    
+    printf("Enter %d elements : ",m*n);
+    for(i=0;i<m;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[ ][ ]);
+    
+   printf("The given matrix is\n");
+    
+          for(i=0;i<m;i++){
+    
+        for(j=0;j<n;j++)
+        {
+            printf("%d ",a[][]);}
+        
+        printf(" ");
+    }   
+    printf("Transpose of the given matrix is\n");
+    
+    for(i=0;i< ;++i)
+    {
+        for(j=0;j< ;++j){
+            printf("%d ",a[ ][ ]);}
+        
+        printf("\n");
+    }
+ return(0);
+          }
